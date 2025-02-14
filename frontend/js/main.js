64db7a49811d5c20613c17220c1ab76612982a42
victor <victorvieira.x@gmail.com>
2025-02-14 00:41:09 -0300

    v0.3 - Fixed css problems and recompiled backend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,3 @@
-const noteIdSrc = document.getElementById("inputTexto").value;
-
 function showBox() {
     let searchBox = document.getElementById("searchBox");
     searchBox.style.display = "flex"; // Exibe a caixa
@@ -9,16 +7,3 @@ function closeBox() {
     let searchBox = document.getElementById("searchBox");
     searchBox.style.display = "none"; // Exibe a caixa
 }
-
-
-document.getElementById('createNoteBtn').addEventListener('click', async () => {
-    const response = await fetch('https://minimumpad.com/tomcat/api/notespi/notes', { method: 'POST' });
-    const { url } = await response.json();
-    window.location.href = `note.html?note=${url}`;
-}); 
-
-
-document.getElementById('searchNoteBtnSend').addEventListener('click', async () => {
-    window.open(`http://localhost/note.html?note=`+document.getElementById("inputTexto").value, '_blank').focus();
-});
-
